8d2114749c0f36cb76543a00dfe55e4583b24476
Jucame <jcmerino@uc.cl>
2023-09-05 23:54:40 -0300

    almost done, errors in encoding and conversion to .rda

--- script.py
+++ script.py
@@ -9,11 +9,33 @@ xlsx_file_name = 'dataset Excel 20230831.xlsx'
 # carga dataframe de xlsx, requiere openpyxl engine
 xlsx_file = pandas.read_excel(os.path.join(in_folder, xlsx_file_name))
 
-# define nombres de archivos convertidos
+# Convierte todas las columnas con tipo int64 a int16
+columns = ['mission_start_month', 'mission_start_year', 'mission_end_month', 'mission_end_year', 
+           'appointment', 'region_2', 
+           'mandate_start_month', 'mandate_start_year', 'mandate_end_month', 'mandate_end_year']
+
+for column in columns:
+    xlsx_file[column] = xlsx_file[column].astype('int16')
+
+# Convierte todas las columnas tipo object a str
+columns_2 = ['mission', 'country', 'region', 'appointment_2', 'last_name', 'name', 'gender', 'nationality']
+
+for column in columns_2:
+    xlsx_file[column] = xlsx_file[column].astype(str)
+
+print(xlsx_file.dtypes)
+print(xlsx_file['country'])
+
+# define nombres de archivos convertidos y ruta de salida
 base_file_name = 'dataset 20230831'
+# DTA (stata full)
 dta_file_name = base_file_name + '.dta'
-rda_file_name = base_file_name + '.rda'
+dta_file_path = os.path.join(out_folder, dta_file_name)
+# TAB (stata limited)
 tab_file_name = base_file_name + '.tab'
+tab_file_path = os.path.join(out_folder, tab_file_name)
 
-# convierte a .dta, .rda y .tab en "out"
-
+# convierte a .dta
+xlsx_file.to_stata(dta_file_path, write_index = False, version=118)
+# .tab
+xlsx_file.to_csv(tab_file_path, sep='\t', index=False)
